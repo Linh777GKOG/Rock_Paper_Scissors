139a8dc8bd5b30ded62973b8db638f24d9ee699d
Linh Vu <linh777gkog@gmail.com>
2022-06-19 23:41:25 +0700

    make desicion

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ const SELECTIONS = [
     beats: 'paper',
   },
 ];
+
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener('click', (e) => {
+    const selectionName = selectionButton.dataset.selection;
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === selectionName
+    );
+    makeSelection(selection);
+  });
+});
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection();
+  const yourWinner = isWinner(selection, computerSelection);
+  const computerWinner = isWinner(computerSelection, selection);
+
+  addSelectionResult(computerSelection, computerWinner);
+  addSelectionResult(selection, yourWinner);
+
+  if (yourWinner) incrementScore(yourScoreSpan);
+  if (computerWinner) incrementScore(computerScoreSpan);
+}
