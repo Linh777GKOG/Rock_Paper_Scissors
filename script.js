6b1f7b67c9fa2cc3dd773a9b694cd6ca0c3ce03d
Linh Vu <linh777gkog@gmail.com>
2022-06-19 23:42:07 +0700

    push code

--- script.js
+++ script.js
@@ -41,3 +41,24 @@ function makeSelection(selection) {
   if (yourWinner) incrementScore(yourScoreSpan);
   if (computerWinner) incrementScore(computerScoreSpan);
 }
+
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner) {
+  const div = document.createElement('div');
+  div.innerText = selection.emoji;
+  div.classList.add('result-selection');
+  if (winner) div.classList.add('winner');
+  finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name;
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
+}
